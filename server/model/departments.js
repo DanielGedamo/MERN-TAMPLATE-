eff146767be9df26a55392223c0a59fc81cf30c7
DANIELGEDAMO <danielgadamo@gmail.com>
2022-12-02 00:00:04 +0200

    l

--- server/model/departments.js
+++ server/model/departments.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 
 const departmentsSchema = new mongoose.Mongoose.Schema({
-    
+    Quantity:Number
 })
+module.exports = mongoose.model("departmentsSchema",departmentsSchema)
